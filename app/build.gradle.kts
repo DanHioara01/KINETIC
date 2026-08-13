a53c215378189cfb6756f033b40c4749aea5742d
DanHioara <dan.hioara2@gmail.com>
2026-08-14 00:25:14 +0300

    chore: bump version to 2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.kinetic"
         minSdk = 24
         targetSdk = 35
-        versionCode = 22
-        versionName = "2.2"
+        versionCode = 23
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
